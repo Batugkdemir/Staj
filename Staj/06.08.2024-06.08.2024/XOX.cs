0f485d8350d9ecd0f8a5cec123dc89307fae2f24
Batuhan Gökdemir <slaices23@gmail.com>
2024-10-16 01:19:56 +0300

    Update XOX.cs

--- Staj/06.08.2024-06.08.2024/XOX.cs
+++ Staj/06.08.2024-06.08.2024/XOX.cs
@@ -1,70 +1,104 @@
 using System;
-class HelloWorld {
-  static void Main() {
-    char[,] numbers = {{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}};
-    do 
+
+class HelloWorld
+{
+    static void Main()
     {
-        //Hamle Algoritması
-      try
-      {
-        Console.WriteLine("Hamle İçin Sütun Yazınız.");
-        int sutun = int.Parse(Console.ReadLine());
-        Console.WriteLine("Hamle İçin Satır Yazınız.");
-        int satır = int.Parse(Console.ReadLine());
-        Console.WriteLine("Hamle Yazınız (X or O).");
-        char hamle = Console.ReadKey().KeyChar;
-        if(numbers[satır-1,sutun-1]==' ')
+        char[,] numbers = { { ' ', ' ', ' ' }, { ' ', ' ', ' ' }, { ' ', ' ', ' ' } };
+        do
         {
-            if(hamle == 'X' || hamle == 'O')
+            try
             {
-                numbers[satır-1,sutun-1]=hamle;
-                if (CheckWin(numbers, hamle))
+                
+                Console.WriteLine("Hamle İçin Sütun Yazınız.");
+                int sutun = int.Parse(Console.ReadLine());
+                Console.WriteLine("Hamle İçin Satır Yazınız.");
+                int satır = int.Parse(Console.ReadLine());
+                Console.WriteLine("Hamle Yazınız (X or O).");
+                char hamle = Console.ReadKey().KeyChar;
+                Console.WriteLine(); 
+
+                if (numbers[satır - 1, sutun - 1] == ' ')
                 {
-                    Console.Clear();
-                    DrawBoard(numbers);
-                    Console.WriteLine($"Tebrikler! {hamle} kazandı!");
-                    break;
+                    if (hamle == 'X' || hamle == 'O')
+                    {
+                        numbers[satır - 1, sutun - 1] = hamle;
+
+                        
+                        if (CheckWin(numbers, hamle))
+                        {
+                            Console.Clear();
+                            DrawBoard(numbers);
+                            Console.WriteLine($"Tebrikler! {hamle} kazandı!");
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Lütfen X veya O Giriniz");
+                    }
                 }
+                else
+                {
+                    Console.WriteLine("\nLütfen Boş Bir Yer Seçiniz.");
+                }
+
+                Console.WriteLine("-----------------------------");
+                DrawBoard(numbers);
             }
-            else
+            catch (FormatException ex)
             {
-                Console.WriteLine("Lütfen X veya O Giriniz");
+                Console.WriteLine("Lütfen Sayıyla Giriş Yapınız.");
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("\nLütfen İstenilen Değerlerden Birini Giriniz.");
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("\nLütfen İstenilen Değerlerden Birini Giriniz.");
             }
         }
-        else
-        {
-            Console.WriteLine("\nLütfen Boş Bir Yer Seçiniz.");
-        }
-        Console.WriteLine("-----------------------------");
-        //numbers[satır-1,sutun-1]=hamle;
+        while (true);
+    }
+
+    static void DrawBoard(char[,] numbers)
+    {
         Console.Write("  1  2  3\n");
-        for(int i=0;i<3;i++){
-            Console.Write(i+1);
-            for(int j=0;j<3;j++){ 
-                 Console.Write( " "+numbers[i,j]+" ");
+        for (int i = 0; i < 3; i++)
+        {
+            Console.Write(i + 1);
+            for (int j = 0; j < 3; j++)
+            {
+                Console.Write(" " + numbers[i, j] + " ");
             }
             Console.Write("\n");
         }
-      }
-      catch(FormatException ex){Console.WriteLine("Lütfen Sayıyla Giriş Yapınız.");}
-      catch(IndexOutOfRangeException ex){Console.WriteLine("\nLütfen İstenilen Değerlerden Birini Giriniz.");}
-      catch(OverflowException ex){Console.WriteLine("\nLütfen İstenilen Değerlerden Birini Giriniz.");}
-        // 1.Satır
-       /* if(sutun == sutun1 && satır == satır1){Console.Write(numbers[4,0]);}
-        if(sutun == sutun2 && satır == satır1){Console.Write(numbers[4,1]);}
-        if(sutun == sutun3 && satır == satır1){Console.Write(numbers[4,2]);}
-        // 2.Satır
-        //if(sutun == sutun1 && satır == satır2){sutundegeri=numbers[4,0];}
-        //if(sutun == sutun2 && satır == satır2){sutundegeri2=numbers[4,1];}
-        //if(sutun == sutun3 && satır == satır2){sutundegeri3=numbers[4,2];}
-       
-        Console.Write("   "+numbers[0,1]);
-        Console.Write("   "+numbers[0,2]+"\n");
-        Console.WriteLine("\n"+numbers[1,0]+"  "+sutundegeri+"   "+sutundegeri2+"   "+sutundegeri3+"\n");
-        Console.WriteLine(numbers[1,1]+"\n");
-        Console.WriteLine(numbers[1,2]+"\n");*/
     }
-    while (true);
-    
-  }
+
+    static bool CheckWin(char[,] board, char player)
+    {
+        
+        for (int i = 0; i < 3; i++)
+        {
+            if (board[i, 0] == player && board[i, 1] == player && board[i, 2] == player)
+                return true;
+        }
+
+        
+        for (int i = 0; i < 3; i++)
+        {
+            if (board[0, i] == player && board[1, i] == player && board[2, i] == player)
+                return true;
+        }
+
+        
+        if (board[0, 0] == player && board[1, 1] == player && board[2, 2] == player)
+            return true;
+
+        if (board[0, 2] == player && board[1, 1] == player && board[2, 0] == player)
+            return true;
+
+        return false;
+    }
 }
